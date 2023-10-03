cba19762dad260c865cdaa9064e1a6bc2d34d53b
RyanBHuynh <ryanhuynh@mu-586068.dhcp.missouri.edu>
2023-10-03 10:39:07 +0200

    add main.py

---
+++ main.py
@@ -0,0 +1,10 @@
+# INF264 Project 2
+# Phillip Lei and Ryan Huynh
+
+# Import libraries
+import numpy as np
+
+# Main
+if __name__ == "__main__":
+    X = np.load("emnist_hex_images.npy")
+    y = np.load("emnist_hex_labels.npy")
