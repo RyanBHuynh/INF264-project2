f366d724e1e8ef31614334880995f8ccc9e5c44f
RyanBHuynh <ryanhuynh@mu-586068.dhcp.missouri.edu>
2023-10-03 12:41:32 +0200

    add function to visualize an individual pixel

--- main.py
+++ main.py
@@ -3,8 +3,16 @@
 
 # Import libraries
 import numpy as np
+import matplotlib.pyplot as plt
+
+# Functions
+def visualize_image(X_entry):
+    plt.imshow(X_entry.reshape(20,20), vmin=0, vmax=255, cmap="gray")
+    plt.show()
 
 # Main
 if __name__ == "__main__":
     X = np.load("emnist_hex_images.npy")
     y = np.load("emnist_hex_labels.npy")
+
+    visualize_image(X[0])
