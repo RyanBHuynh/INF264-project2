9efab31b1ed17b041e5272ec4e96a69f7e1b67b9
Phillip Lei <plgrx@umsystem.edu>
2023-10-12 13:39:13 +0200

    more function headers, wrote todo steps in main

--- main.py
+++ main.py
@@ -14,6 +14,7 @@ import matplotlib.pyplot as plt
 from sklearn.neural_network import MLPClassifier
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.svm import SVC
+from sklearn.model_selection import train_test_split
 
 # Functions
 def visualize_image(X_entry):
@@ -26,14 +27,44 @@ def visualize_image(X_entry):
     plt.imshow(X_entry.reshape(20,20), vmin=0, vmax=255, cmap="gray")
     plt.show()
 
+"""
+
+"""
+def preprocess_data(X, y):
+    pass
+    
+"""
+Responsible for creating the three candidate models: nn, decision tree, support vector machine.
+
+Output: returns array of the three models
+"""
+def create_all_models():
+    pass
+
+"""
+Creates one of the three candidates: nn
+
+Output: neural network model - sklearn
+"""
 def create_nn(X_train, y_train): 
     pass
 
+"""
+Creates one of the three candidates: decision tree
+
+Output: decision tree model - sklearn
+"""
 def create_dtree(X_train, y_train):
     pass
 
+"""
+Creates one of the three candidates: support vector machine
+
+Output: support vector machine model - sklearn
+"""
 def create_svm(X_train, y_train):
     pass
+
 
 def evaluate_model():
     pass
@@ -45,3 +76,18 @@ def select_best_model():
 if __name__ == "__main__":
     X = np.load("emnist_hex_images.npy")
     y = np.load("emnist_hex_labels.npy")
+    
+    # Step 1. Preprocess Data, do we need this? Eg: making images smaller
+    
+    # Note: fix a seed for reproducibility
+    
+    # Step 2. Split processed data
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, shuffle=True)
+    
+    # Step 3. Create 3 test models
+    
+    # Step 4. Select model
+    
+    # Step 5. Evaluate model
+    
+    # Step 6. Print out numbers like accuracy. Should be reproducible across runs
